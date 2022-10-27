cac45202c198ee001da52862dd594be104d019ce
fakihfavian24 <fakihfavianwibowo@gmail.com>
2022-10-27 14:33:00 +0700

    Menyelesaikan video 5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,56 +30,8 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fakih Favian Wibowo",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ab laborum corporis quidem tempora officiis quisquam magnam quis assumenda fuga quo aliquid praesentium voluptatem temporibus tenetur cumque adipisci ipsam, sit mollitia, architecto natus velit nulla quae quia? Labore dolore reiciendis eos deserunt nobis voluptates mollitia quam perferendis aliquam, aliquid id aperiam nostrum eius explicabo asperiores tempora inventore consectetur et architecto facere cupiditate vero dolor quaerat? Fuga maiores unde veniam pariatur. Rerum repellat ducimus illum dolores? Maxime sequi molestiae labore non."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Anonymous",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis ipsam veniam quia, aliquid aliquam saepe quidem adipisci et, quo modi hic aspernatur nulla quas corrupti? Quo dolor repudiandae vitae, natus culpa modi mollitia delectus itaque ex quibusdam sit enim asperiores, doloremque repellendus. Nostrum vitae molestiae, veniam nulla eos eligendi velit deserunt aliquid corporis voluptatibus quasi mollitia quia. Voluptates sed ea porro. Illo quidem numquam alias necessitatibus architecto ipsum ad quos earum molestias eius eos optio suscipit mollitia aspernatur totam nesciunt ex libero, ratione velit nostrum. Dicta, quas maxime deserunt, quasi consectetur, tempore explicabo cumque illo sapiente odit quaerat iusto blanditiis?"
-        ],
-    ];
-
-
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 
 // halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fakih Favian Wibowo",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ab laborum corporis quidem tempora officiis quisquam magnam quis assumenda fuga quo aliquid praesentium voluptatem temporibus tenetur cumque adipisci ipsam, sit mollitia, architecto natus velit nulla quae quia? Labore dolore reiciendis eos deserunt nobis voluptates mollitia quam perferendis aliquam, aliquid id aperiam nostrum eius explicabo asperiores tempora inventore consectetur et architecto facere cupiditate vero dolor quaerat? Fuga maiores unde veniam pariatur. Rerum repellat ducimus illum dolores? Maxime sequi molestiae labore non."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Anonymous",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis ipsam veniam quia, aliquid aliquam saepe quidem adipisci et, quo modi hic aspernatur nulla quas corrupti? Quo dolor repudiandae vitae, natus culpa modi mollitia delectus itaque ex quibusdam sit enim asperiores, doloremque repellendus. Nostrum vitae molestiae, veniam nulla eos eligendi velit deserunt aliquid corporis voluptatibus quasi mollitia quia. Voluptates sed ea porro. Illo quidem numquam alias necessitatibus architecto ipsum ad quos earum molestias eius eos optio suscipit mollitia aspernatur totam nesciunt ex libero, ratione velit nostrum. Dicta, quas maxime deserunt, quasi consectetur, tempore explicabo cumque illo sapiente odit quaerat iusto blanditiis?"
-        ],
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] === $slug)
-            $new_post = $post;
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts/{slug}', [PostController::class, 'show']);
