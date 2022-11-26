7afc688344cfc7b2d92dceaa532219dd50a354fd
fakihfavian24 <fakihfavianwibowo@gmail.com>
2022-11-26 21:22:01 +0700

    Menyelesaikan Video 21

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        // User::create([
-        //     'name' => 'Fakih Favian Wibowo',
-        //     'email' => 'fakih@gmail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Fakih Favian Wibowo',
+            'username' => 'fakihfavian',
+            'email' => 'fakih@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
 
         // User::create([
         //     'name' => 'Anonymous',
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,7 +11,13 @@
 
       <p>By. <a href="/blog?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/blog?categories={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
+      @if ($post->image)
+      <div style="max-height: 350px; overflow: hidden;">
+        <img src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->category->name }}" class="img-fluid">
+      </div>
+      @else
       <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+      @endif
 
       <article class="my-3 fs-5">
         <!-- <h5>{{ $post->author }}</h5> -->
